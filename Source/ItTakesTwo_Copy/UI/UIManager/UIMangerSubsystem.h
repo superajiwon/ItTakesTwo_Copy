d85e0244c83a75c9de415aabdb7ea3fc924adeb3
권선 <97091329+gwonseon@users.noreply.github.com>
2026-04-20 09:26:41 +0900

    feat: Implement MVVM UI structure with ViewModel and UIManager
    UI 구조를 MVVM 패턴으로 구성하고 ViewModel과 UIManager를 추가
    
    UI와 로직을 분리하여 유지보수성과 확장성을 개선하기 위함

---
+++ Source/ItTakesTwo_Copy/UI/UIManager/UIMangerSubsystem.h
@@ -0,0 +1,42 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "UIMangerSubsystem.generated.h"
+
+
+class ULobbyViewModel;
+class UHUDViewModel;
+
+UCLASS()
+class ITTAKESTWO_COPY_API UUIMangerSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	
+
+	// WBP와 ViewModel은 OpenLevel 할 때마다 재생성하는데 같은 레벨에선 재사용하게 만들 예정
+public: // UI 시작, ViewModel & WBP 생성
+	void BeginLobbyUI();
+	void BeginHUD();
+	
+	
+public: // 레벨 전환 시 바인딩한 ViewModel, WBP 삭제로직
+	void EndLobbyUI();
+	void EndHUD();
+	
+	
+	
+private:
+	UPROPERTY()
+	TObjectPtr<UHUDViewModel> HUDViewModel;
+	
+	UPROPERTY()
+	TObjectPtr<ULobbyViewModel> LobbyViewModel;
+	
+	
+};
---
+++ Source/ItTakesTwo_Copy/UI/ViewModel/LobbyViewModel/LobbyViewModel.cpp
@@ -0,0 +1,17 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LobbyViewModel.h"
+
+void ULobbyViewModel::Initialize(TObjectPtr<APlayerController> PlayerController)
+{
+	Super::Initialize(PlayerController);
+	// 델리게이트  바인드
+// 	MyPlayerController->GetWorld();
+}
+
+void ULobbyViewModel::Deinitialize()
+{
+	// 델리게이트 릴리즈
+	Super::Deinitialize();
+}
