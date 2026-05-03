507dab54beeaa46ab36e73ec7d02bc206d81bb0c
권선 <97091329+gwonseon@users.noreply.github.com>
2026-05-04 01:13:24 +0900

    feat: Add Crusher boss mechanics
    Crusher 보스의 동작 기믹과 이동 로직을 구현
    
    맵 오브젝트 파괴 로직 구현

--- Source/ItTakesTwo_Copy/Private/Actors/Characters/Monsters/Boss/ToyCrusher_Monster.cpp
+++ Source/ItTakesTwo_Copy/Private/Actors/Characters/Monsters/Boss/ToyCrusher_Monster.cpp
@@ -2,26 +2,54 @@
 
 
 #include "Actors/Characters/Monsters/Boss/ToyCrusher_Monster.h"
-#include "Shared/Components/HitBoxComponent.h"
-#include "Shared/Struct/HitComp_Info.h"
 
+#include "Actors/Characters/Players/PlayerBase.h"
+#include "Actors/Map/MapObjectBase.h"
+#include "Actors/Map/MapObject_Attackable.h"
+#include "Actors/Map/MapObject_Button.h"
+#include "Shared/VFXObjectPoolSubsystem.h"
+#include "Shared/Components/HitBoxComponent.h"
+#include "Shared/Struct/FVFXSpawn_Info.h"
+#include "Shared/Struct/HitComp_Info.h"
+#include "Shared/Struct/HitRequest.h"
+#include "Shared/Subsystems/CombatSystem.h"
+
+
+class UVFXObjectPoolSubsystem;
 
 AToyCrusher_Monster::AToyCrusher_Monster()
 {
 	PrimaryActorTick.bCanEverTick = true;
 	
-	MoveSpeed = 300.f;
-	MonsterMoveType = EMonsterMoveType::MoveForward;
-	
-	BoxComponent = CreateDefaultSubobject<UHitBoxComponent>(FName("HitBoxComponent"));
-	BoxComponent->AttachToComponent(GetMesh(), FAttachmentTransformRules::KeepRelativeTransform);
-	BoxComponent->SetBoxExtent(FVector(130.f, 700.f, 570.f));
-	BoxComponent->SetRelativeLocation(FVector::ZeroVector);
-	BoxComponent->SetCollisionProfileName("MonsterWeapon");
-	BoxComponent->SetGenerateOverlapEvents(true);
-	BoxComponent->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
-	BoxComponent->OnComponentBeginOverlap.AddDynamic(this, &AToyCrusher_Monster::OnHitBoxBeginOverlap);
 
+	MoveSpeed = 300.f;
+
+	AIControllerClass = nullptr;
+	AutoPossessAI = EAutoPossessAI::Disabled;
+	MonsterMoveType = EMonsterMoveType::End;
+
+	HitBoxComponent = CreateDefaultSubobject<UHitBoxComponent>(TEXT("HitBoxComponent"));
+	HitBoxComponent->AttachToComponent(GetMesh(), FAttachmentTransformRules::KeepRelativeTransform);
+	HitBoxComponent->SetBoxExtent(FVector(130.f, 700.f, 570.f));
+	HitBoxComponent->SetRelativeLocation(FVector(80.f, 0.f, 628.f));
+	HitBoxComponent->SetCollisionProfileName(FName(TEXT("MonsterWeapon")));
+	HitBoxComponent->SetGenerateOverlapEvents(false);
+	HitBoxComponent->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	HitBoxComponent->SetDamage(static_cast<int32>(ImpactDamage));
+
+	HitBoxComponent->OnComponentBeginOverlap.AddDynamic(
+		this,
+		&AToyCrusher_Monster::OnHitBoxBeginOverlap
+	);
+
+	static ConstructorHelpers::FObjectFinder<UNiagaraSystem> Asset(
+		TEXT("/Game/VFX/Using/NS_CrusherImpact.NS_CrusherImpact")
+	);
+
+	if (Asset.Succeeded())
+	{
+		ImpactNiagara = Asset.Object;
+	}
 }
 
 void AToyCrusher_Monster::BeginPlay()
@@ -33,14 +61,120 @@ void AToyCrusher_Monster::BeginPlay()
 void AToyCrusher_Monster::OnHitBoxBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
 	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	
+	if (!HasAuthority())
+		return;
+
+	if (!bActive)
+		return;
+
+	if (!OtherActor || OtherActor == this)
+		return;
+	if (Cast<AMapObject_Button>(OtherActor))
+		OtherActor->Destroy();
+	const bool bIsPlayer = Cast<APlayerBase>(OtherActor) != nullptr;
+	const bool bIsAttackableMapObject = Cast<AMapObject_Attackable>(OtherActor) != nullptr;
+
+	if (!bIsPlayer && !bIsAttackableMapObject)
+		return;
+
+	UCombatSystem* CombatSystem = GetWorld()->GetSubsystem<UCombatSystem>();
+	if (!CombatSystem)
+		return;
+
+	const FVector HitLocation =OtherActor->GetActorLocation();
+
+	FHitRequest Request(
+		this,
+		OtherActor,
+		static_cast<int32>(ImpactDamage),
+		HitLocation
+	);
+
+	CombatSystem->ProcessHit(Request);
+
 	bOverlapedToTarget = true;
-	
+}
+
+void AToyCrusher_Monster::AnimNotify_CrusherAttack()
+{
+	if (!HasAuthority())
+		return;
+
+	if (!ImpactNiagara)
+		return;
+
+	UVFXObjectPoolSubsystem* PoolSubsystem = GetWorld()->GetSubsystem<UVFXObjectPoolSubsystem>();
+	if (!PoolSubsystem)
+		return;
+
+	const FVector SpawnLocation = GetActorLocation();
+
+	FVFXSpawn_Info SpawnInfo = FVFXSpawn_Info::CreateExplosionOnce(
+		this,
+		ImpactNiagara,
+		SpawnLocation
+	);
+
+	SpawnInfo.WithBoxCollision(
+	true,
+	FName(TEXT("MonsterWeapon")),
+	ImpactDamage,
+	ImpactExtents
+);
+
+	PoolSubsystem->UseVFX_Explosion(SpawnInfo);
 }
 
 void AToyCrusher_Monster::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	if (!HasAuthority())
+		return;
+
+	if (!bActive)
+	{
+		const bool bFirstPressed =
+			First_Button &&
+			First_Button->GetMapObjectState() == EMapObjectState::AfterPressed;
+
+		const bool bSecondPressed =
+			Second_Button &&
+			Second_Button->GetMapObjectState() == EMapObjectState::AfterPressed;
+
+		if (bFirstPressed && bSecondPressed)
+		{
+			bActive = true;
+			SetMonsterState(EMonsterState::Chase);
+
+			if (HitBoxComponent)
+			{
+				HitBoxComponent->SetGenerateOverlapEvents(true);
+				HitBoxComponent->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+				HitBoxComponent->UpdateOverlaps();
+			}
+		}
+	}
+
+	if (!bActive)
+		return;
+
+	const FVector DeltaMove = GetActorForwardVector() * MoveSpeed * DeltaTime;
+	AddActorWorldOffset(DeltaMove, true);
+
+	if (GetOverlapToTarget())
+	{
+		if (GetMonsterState() != EMonsterState::Swing)
+		{
+			SetMonsterState(EMonsterState::Swing);
+		}
+	}
+	else
+	{
+		if (GetMonsterState() != EMonsterState::Chase)
+		{
+			SetMonsterState(EMonsterState::Chase);
+		}
+	}
 }
 
 
---
+++ Source/ItTakesTwo_Copy/Public/Actors/Characters/Monsters/WeaponMonsterBase/AnimNotifyState_MageTeleportMove.h
@@ -0,0 +1,54 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotifyState.h"
+#include "AnimNotifyState_MageTeleportMove.generated.h"
+
+UENUM(BlueprintType)
+enum class EMageTeleportMoveMode : uint8
+{
+	Enter,
+	Exit
+};
+
+UCLASS()
+class ITTAKESTWO_COPY_API UAnimNotifyState_MageTeleportMove : public UAnimNotifyState
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Teleport")
+	EMageTeleportMoveMode MoveMode = EMageTeleportMoveMode::Enter;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Teleport")
+	float OffsetZ = -300.f;
+
+	virtual void NotifyBegin(
+		USkeletalMeshComponent* MeshComp,
+		UAnimSequenceBase* Animation,
+		float TotalDuration,
+		const FAnimNotifyEventReference& EventReference
+	) override;
+
+	virtual void NotifyTick(
+		USkeletalMeshComponent* MeshComp,
+		UAnimSequenceBase* Animation,
+		float FrameDeltaTime,
+		const FAnimNotifyEventReference& EventReference
+	) override;
+
+	virtual void NotifyEnd(
+		USkeletalMeshComponent* MeshComp,
+		UAnimSequenceBase* Animation,
+		const FAnimNotifyEventReference& EventReference
+	) override;
+	
+	
+private:
+	FVector StartLocation;
+	FVector TargetLocation;
+	float ElapsedTime = 0.f;
+	float Duration = 0.f;
+};
